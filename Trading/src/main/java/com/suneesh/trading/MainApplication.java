f5b1a115a6a04bc668a847f42d4c78d3ee419a22
Suneesh Joshi <suneesh.joshi@gmail.com>
2020-01-12 23:11:25 +0000

    Adding multiple API calls , and authorization-dependant API calls.

--- Trading/src/main/java/com/suneesh/trading/MainApplication.java
+++ Trading/src/main/java/com/suneesh/trading/MainApplication.java
@@ -1,5 +1,9 @@
 package com.suneesh.trading;
 
+import com.suneesh.trading.models.requests.AccountStatusRequest;
+import com.suneesh.trading.models.requests.AuthorizeRequest;
+import com.suneesh.trading.models.requests.BalanceRequest;
+import com.suneesh.trading.models.requests.TickRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,5 +13,34 @@ public class MainApplication {
 
     public static void main(String[] args) {
         logger.info("Main Application");
+        ApiWrapper api = ApiWrapper.build("21829");
+
+        TickRequest request = new TickRequest("R_10");
+//
+//        api.sendRequest(request)
+//                .subscribe(
+//                        response -> {
+//                            logger.info(String.valueOf(response));
+//
+////                            System.out.printf("epoch: %S, price: %s",
+////                                    response.getTick().getEpoch(), responce.getTick().getQuote());
+//                        }
+//                );
+
+        logger.info("Going to make another call.");
+
+        AuthorizeRequest authorizeRequest = new AuthorizeRequest("9s5aGYbnsUQr3Fv");
+        api.sendRequest(authorizeRequest).subscribe( response -> {
+            api.sendRequest(new BalanceRequest(true));
+        });
+//
+//
+//        AccountStatusRequest accountStatusRequest = new AccountStatusRequest();
+//        api.sendRequest(accountStatusRequest);
+//
+//        api.sendRequest(new BalanceRequest(true));
+
+
+
     }
 }
