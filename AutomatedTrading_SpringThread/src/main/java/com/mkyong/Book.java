a854625448632ce4f47e7319f880e1358c9597bb
Suneesh Joshi <suneesh.joshi@gmail.com>
2020-01-14 23:44:03 +0000

    Adding files and working source code

--- AutomatedTrading_SpringThread/src/main/java/com/mkyong/Book.java
+++ AutomatedTrading_SpringThread/src/main/java/com/mkyong/Book.java
@@ -1,11 +1,14 @@
 package com.mkyong;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Book {
 
     @Id
@@ -20,27 +23,4 @@ public class Book {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- AutomatedTrading_SpringThread/src/main/java/com/mkyong/StartApplication.java
+++ AutomatedTrading_SpringThread/src/main/java/com/mkyong/StartApplication.java
@@ -1,7 +1,7 @@
 package com.mkyong;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,13 +10,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartApplication implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
+    private static final Logger log = LogManager.getLogger();
 
     @Autowired
-    private BookRepository repository;
+    private BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
+    }
+
+    private void oldExampleCode(){
+        Book java = new Book("Java");
+        bookRepository.save(java);
+        bookRepository.save(new Book("Node"));
+        bookRepository.save(new Book("Python"));
+
+        System.out.println("\nfindAll()");
+        bookRepository.findAll().forEach(x -> System.out.println(x));
+
+        System.out.println("\nfindById(1L)");
+        bookRepository.findById(1l).ifPresent(x -> System.out.println(x));
+
+        System.out.println("\nfindByName('Node')");
+        bookRepository.findByName("Node").forEach(x -> System.out.println(x));
     }
 
     @Override
@@ -24,18 +40,13 @@ public class StartApplication implements CommandLineRunner {
 
         log.info("StartApplication...");
 
-        repository.save(new Book("Java"));
-        repository.save(new Book("Node"));
-        repository.save(new Book("Python"));
+        oldExampleCode();
+        MainApplication mainApplication = new MainApplication();
+        mainApplication.init(bookRepository);
 
-        System.out.println("\nfindAll()");
-        repository.findAll().forEach(x -> System.out.println(x));
+        mainApplication.threadCreation();
 
-        System.out.println("\nfindById(1L)");
-        repository.findById(1l).ifPresent(x -> System.out.println(x));
-
-        System.out.println("\nfindByName('Node')");
-        repository.findByName("Node").forEach(x -> System.out.println(x));
+        bookRepository.save(new Book("Python 2"));
 
     }
 
