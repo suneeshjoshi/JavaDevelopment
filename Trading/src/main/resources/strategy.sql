bd0dd0c4292cb3ebfb0ca90ded92aa458f8d0e69
SuneeshJoshi <suneesh.joshi@gmail.com>
2020-01-23 23:41:00 +0000

    Adding strategy and strategy_steps table details

--- Trading/src/main/resources/strategy.sql
+++ Trading/src/main/resources/strategy.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS public.strategy
     max_steps INTEGER NOT NULL,
     CONSTRAINT strategy_pkey PRIMARY KEY (identifier)
 )
+
+--INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps) 	VALUES (1, 'Multiple of 2.5 of last bid amount', now(), 5);
+--INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps) VALUES (2, '2.5 Multiple Strategys 6th step amount divided in £10 Bid, and also recovering all the 10 - £1 bids', now(), 11);
+
+--Helpful Query
+--select * From strategy_steps ss join strategy s on ( ss.strategy_id = s.identifier)
+
--- Trading/src/main/resources/strategy_steps.sql
+++ Trading/src/main/resources/strategy_steps.sql
@@ -4,5 +4,26 @@ CREATE TABLE IF NOT EXISTS public.strategy_steps
     strategy_id INTEGER ,
     step_count integer NOT NULL,
     value numeric(19,4) NOT NULL,
-    CONSTRAINT strategy_steps_pkey PRIMARY KEY (identifier)
-)
+    CONSTRAINT strategy_steps_pkey PRIMARY KEY (identifier),
+);
+
+
+-- POPULATE THE strategy_steps table
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (1, 1, 1, 1.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (2, 1, 2, 2.50);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (3, 1, 3, 6.25);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (4, 1, 4, 15.63);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (5, 1, 5, 39.07);
+--
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (6, 2, 1, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (7, 2, 2, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (8, 2, 3, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (9, 2, 4, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (10, 2, 5, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (11, 2, 6, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (12, 2, 7, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (13, 2, 8, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (14, 2, 9, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (15, 2, 10, 10.00);
+--INSERT INTO public.strategy_steps(identifier, strategy_id, step_count, value)VALUES (16, 2, 11, 10.00);
+
