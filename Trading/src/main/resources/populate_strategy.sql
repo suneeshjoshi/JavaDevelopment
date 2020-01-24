1ef5ab82be0131f36f3618f232420140b10d15ef
Suneesh Joshi <suneesh.joshi@gmail.com>
2020-01-24 10:31:27 +0000

    Adding next_strategy_id_link

--- Trading/src/main/resources/populate_strategy.sql
+++ Trading/src/main/resources/populate_strategy.sql
@@ -1,5 +1,5 @@
 DELETE FROM public.strategy WHERE identifier in ( 1,2 );
 
-INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps) VALUES (1, 'Multiple of 2.5 of last bid amount', now(), 5);
-INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps) VALUES (2, '2.5 Multiple Strategys 6th step amount divided in £10 Bid, and also recovering all the 10 - £1 bids', now(), 11);
+INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps, next_strategy_id_link) VALUES (1, 'Multiple of 2.5 of last bid amount', now(), 5, 2);
+INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps, next_strategy_id_link) VALUES (2, '2.5 Multiple Strategys 6th step amount divided in £10 Bid, and also recovering all the 10 - £1 bids', now(), 11, 1);
 
--- Trading/src/main/resources/strategy.sql
+++ Trading/src/main/resources/strategy.sql
@@ -4,11 +4,9 @@ CREATE TABLE IF NOT EXISTS public.strategy
     strategy_name character varying(255) ,
     creation_date timestamp NOT NULL,
     max_steps INTEGER NOT NULL,
+    next_strategy_id_link INTEGER,
     CONSTRAINT strategy_pkey PRIMARY KEY (identifier)
 )
-
---INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps) 	VALUES (1, 'Multiple of 2.5 of last bid amount', now(), 5);
---INSERT INTO public.strategy( identifier, strategy_name, creation_date, max_steps) VALUES (2, '2.5 Multiple Strategys 6th step amount divided in £10 Bid, and also recovering all the 10 - £1 bids', now(), 11);
 
 --Helpful Query
 --select * From strategy_steps ss join strategy s on ( ss.strategy_id = s.identifier)
