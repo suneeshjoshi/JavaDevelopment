9e6662eeaba5b88986673b4c8e7f7ea4dccfe0aa
Suneesh Joshi <suneesh.joshi@gmail.com>
2020-02-03 14:57:20 +0000

    Adding new Application monitor , to ensure the automated Trading software is always running. If the software experiences problem or crashes , then this Applicationmonitor will spawn a new instance of the trading software

---
+++ AutoTradingApplicationMonitor/src/main/java/com/suneesh/trading/monitor/MainApplication.java
@@ -0,0 +1,10 @@
+package com.suneesh.trading.monitor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainApplication {
+    public static void main(String[] args) {
+        log.info("Starting Automated Trading Application monitor");
+    }
+}
