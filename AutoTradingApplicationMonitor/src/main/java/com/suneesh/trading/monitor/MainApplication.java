ba9037932ea2ad40fd41c7b4602490023b2f7712
SuneeshJoshi <suneesh.joshi@gmail.com>
2020-02-04 20:46:40 +0000

    syncing from laptop to desktop

--- AutoTradingApplicationMonitor/src/main/java/com/suneesh/trading/monitor/MainApplication.java
+++ AutoTradingApplicationMonitor/src/main/java/com/suneesh/trading/monitor/MainApplication.java
@@ -2,7 +2,10 @@ package com.suneesh.trading.monitor;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -12,7 +15,18 @@ public class MainApplication {
         log.info("Starting application count = {}",++count);
         Process process = null;
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("java -jar ");
+        Path pathToApplication = Paths.get("C:\\Windows\\System32");
+//        String application ="notepad.exe";
+        String commandToRun = "";
+//
+//        Path pathToApplication = Paths.get("C:\\Windows\\System32");
+        String application ="runAutoTradingWindows.bat";
+//        String commandToRun = "";
+
+//        String finalCommand = commandToRun + " "+ pathToApplication + "\\" + application;
+        String finalCommand = application;
+
+        processBuilder.command(finalCommand);
 
         try {
             process = processBuilder.start();
