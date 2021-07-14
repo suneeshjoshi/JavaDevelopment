94bc4e286d5c18372ed21587811fe1630909512f
Suneesh Joshi <suneesh.joshi@gmail.com>
2021-07-14 22:59:35 +0100

    Adding AOP for @Before and various Pointcut , Order annotations

--- SpringForLSEG/SpringAOP/src/main/java/com/suneesh/aopdemo/aspect/MyDemoLoggingAspect.java
+++ SpringForLSEG/SpringAOP/src/main/java/com/suneesh/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,17 +1,25 @@
 package com.suneesh.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
+    @Pointcut("execution(* com.suneesh.aopdemo.*.*.*Account(..))")
+    private void sharePointcutExpression(){}
+
 //    @Before("execution(public void addAccount())")
-    @Before("execution(* com.suneesh.aopdemo.*.*.*Account(..))")
-    public void actionForBeforeAddAccount(){
-        System.out.println(getClass() + "   ====================>>> Going to call *Account()");
+    @Before("sharePointcutExpression()")
+    public void actionForBeforeAddAccount(JoinPoint joinPoint){
+        System.out.println(getClass() + "   ====================>>> Going to call *Account()" + joinPoint.getSignature() );
+        for(Object arg : joinPoint.getArgs()){
+            System.out.println(arg);
+        }
     }
 
 }
